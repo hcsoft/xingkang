61198ed3c0929448adec53de9a57b4d8b9f34ea0
payallmoney <payallmoney@163.com>
2014-11-11 00:24:51 +0800

    1,修改了消费汇总无会员数据的问题,改成左关联

--- admin/control/member.php
+++ admin/control/member.php
@@ -340,8 +340,9 @@ class memberControl extends SystemControl {
 		$page = new Page ();
 		$page->setEachNum ( 10 );
 		$page->setNowPage ( $_REQUEST ["curpage"] );
-		$sql = 'from Center_CheckOut a  , Center_codes ico, Center_codes gather,Center_codes state,Center_codes tag,
-             Organization org left join  Center_MemberInfo member on a.sMemberID = member.sMemberID
+		$sql = 'from Center_CheckOut a  left join  Center_MemberInfo member on a.sMemberID = member.sMemberID
+		    , Center_codes ico, Center_codes gather,Center_codes state,Center_codes tag,
+             Organization org
           where a.iCO_Type = ico.code and ico.type=\'iCO_Type\'
            and  a.iCO_GatherType = gather.code and gather.type=\'iCO_GatherType\'
            and  a.iCO_State = state.code and state.type=\'iCO_State\'
