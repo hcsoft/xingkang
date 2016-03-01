2c273099511fa1bba8aa48a7f11c3066323adf3e
oyx <44550777@qq.com>
2016-03-01 10:09:36 +0800

    1,增加了整体业务

--- admin/include/menu.php
+++ admin/include/menu.php
@@ -69,6 +69,7 @@ $arr = array(
 				'list' => array(
 					array('args'=>'welcome,dashboard,dashboard',			'text'=>'系统总览'),
 					array('args'=>'chartpage,dashboard,dashboard',			'text'=>'图表'),
+					array('args'=>'welcome,dashboardnew,dashboard',			'text'=>'整体业务'),
 //					array('args'=>'aboutus,dashboard,dashboard',			'text'=>$lang['nc_aboutus']),
 //					array('args'=>'base,setting,dashboard',	'text'=>$lang['nc_web_set']),
 //					array('args'=>'member,member,dashboard',				'text'=>$lang['nc_member_manage']),
